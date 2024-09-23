d41b28267e0fa4e1a977102836220089e2a7880e
Elwafi-Elmehdi <elwafi.courrier99@gmail.com>
2024-09-23 20:09:55 +0100

    Updated terraform | Added/imported netlify dns records

---
+++ terraform/homelab/netlify/import.tf
@@ -0,0 +1,9 @@
+import {
+  id = "66ea09314e4b6eaa7a6280d3:66eac4275c98a774d605d692"
+  to = netlify_dns_record.proxmox1_record
+}
+
+import {
+  id = "66ea09314e4b6eaa7a6280d3:66eac44368041b007b2e287a"
+  to = netlify_dns_record.prometheus_record
+}
---
+++ terraform/homelab/netlify/variables.tf
@@ -0,0 +1,5 @@
+variable "netlify_api_token" {
+  type        = string
+  sensitive   = true
+  description = "The API access key to your netlify account"
+}
